6afca10b6e2d6e82b724bdbf3eca6cfe31009b9a
housssem1999 <mhoussem789@gmail.com>
2023-02-16 10:33:01 +0100

    version 11

--- web/src/app/delete-customer/delete-customer.component.spec.ts
+++ web/src/app/delete-customer/delete-customer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DeleteCustomerComponent } from './delete-customer.component';
 
@@ -6,7 +6,7 @@ describe('DeleteCustomerComponent', () => {
   let component: DeleteCustomerComponent;
   let fixture: ComponentFixture<DeleteCustomerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DeleteCustomerComponent ]
     })
--- web/src/app/update-customer/update-customer.component.spec.ts
+++ web/src/app/update-customer/update-customer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UpdateCustomerComponent } from './update-customer.component';
 
@@ -6,7 +6,7 @@ describe('UpdateCustomerComponent', () => {
   let component: UpdateCustomerComponent;
   let fixture: ComponentFixture<UpdateCustomerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UpdateCustomerComponent ]
     })
